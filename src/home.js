01d408d8312dd00079f358dffe55a3ea36bb21d0
burczu <b.dybowski@gmail.com>
2017-06-25 12:21:30 +0200

    css loader + sass loader

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
 import { x } from './libs/lib';
+import './style.css';
+import './style.scss';
 
 console.log(x);
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
             presets: ['env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
       }
     ]
   }
